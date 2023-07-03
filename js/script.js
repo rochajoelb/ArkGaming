ae9e8bcd2815396bd2b7a7d4429e804adf96e1ee
Joel Rocha <joelrochaupskill@gmail.com>
2023-07-04 00:17:10 +0100

    junçao das areas de e-games e boardgames

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+ /*
  $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
@@ -6,37 +7,17 @@
 
 });
  
-// Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
-
-/*--------------------------------------------------------------------*/
-/*
-function transform1(){
-  document.getElementById('img1').style.transform = 'scale(0)'
-  document.getElementById('img2').style.transform = 'scale(1)'
-}
-
-function transform2(){
-  document.getElementById('img2').style.transform = 'scale(0)'
-  document.getElementById('img1').style.transform = 'scale(1)'
-}
-
-function transformBody(){
-  document.getElementById('img2').style.transform = 'scale(1)'
-  document.getElementById('img1').style.transform = 'scale(1)'
-}
 */
+$('.navTrigger').click(function () {
+  $(this).toggleClass('active');
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  
+  if ($("#mainListDiv").hasClass("show_list")) {
+    $("#mainListDiv").fadeIn();
+  } else {
+    $("#mainListDiv").fadeOut();
+  }
+});
+
+
