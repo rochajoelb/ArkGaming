326b8948e44b496b130a7e4b731a50ec4db9f2c4
Joel Rocha <joelrochaupskill@gmail.com>
2023-07-08 15:16:13 +0100

    html,css e script do carrinho  integrado no site

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ function guardaEmail(){
   if(checkBox.checked){
     const guardarEmail = document.getElementById("emailInput").value;
     if(guardaEmail !== ""){
-      fetch("http://localhost:4000/store-email", {
+      fetch("http://localhost:3000/store-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -58,10 +58,91 @@ function guardaEmail(){
   }
 }
 
+/****************************funções para controlar o carrinho de compras********************************** */
+
+//A primeira versão utilizava o server em node js para recolher os artigos para um ficheiro json e posteriormente enviar para localstorage
+//Esta é a segunda versão fazendo a atualização diretamente no localstorage que permite a remoçao e dos artigos no carrinho
+
+function adicionarAoCarrinho(nome, preco) {
+  var prod = {};
+  if (nome !== "" && preco !== "") {
+    prod = { nome: nome, preco: preco };
+  }
+  
+  var produtos = [];
+  var produtosLocalStorage = window.localStorage.getItem("produtos"); //busca o json file que já esta em localstorage
+
+  if (produtosLocalStorage !== null) {
+    produtos = JSON.parse(produtosLocalStorage); //faz o parse para a lista
+  }
+  try{
+    produtos.push(prod); //incrementa a lista com o novo produto 
+    window.localStorage.setItem("produtos", JSON.stringify(produtos)); //envia a lista para localstorage novamente
+    alert("Produto adicionado ao carrinho");
+  }catch{
+    alert("Algo correu mal! Não foi possivel adicionar o produto!")
+  }
+}
 
 
+function carrinho() {
+  const produtos = JSON.parse(window.localStorage.getItem("produtos"));
+
+  let carrinhoTableBody = document.querySelector("#carrinhoTable tbody");
+  let total = 0;
+
+  for (var i = 0; i < produtos.length; i++) {
+    var produto = produtos[i];
+
+    var novaLinha = document.createElement("tr");
+    novaLinha.innerHTML = `
+      <td>${produto.nome}</td>
+      <td>${produto.preco}</td>
+      <td><input id="qtd" type="number" value="1"></td>
+      <td><button class="item" onclick="removerItem(this)">&times</button></td>
+    `;
+    carrinhoTableBody.appendChild(novaLinha);
+    total += parseFloat(produto.preco);
+  }
+  
+  var campoTotal = document.querySelector("#campoTotal");
+  campoTotal.innerHTML = ` ${total.toFixed(2)}`;
+}
 
 
+function limparCarrinho() {
+  localStorage.removeItem("produtos");
+  var carrinhoTableBody = document.querySelector("#carrinhoTable tbody");
+  carrinhoTableBody.innerHTML = "";
+  var total = document.querySelector("#campoTotal");
+  total.innerHTML = "";
+
+  alert("Obrigado pela sua compra!");
+}
+
+
+//funcçao incompleta nao estou a conseguir atualizar o 
+//total depois de remover um artigo sem recarregar a pagina manualmente
+
+function removerItem(){
+  var artigo = document.querySelector('.item');
+  var produtosLocalStorage = localStorage.getItem("produtos");
+  
+  var removeProduto = artigo.parentNode.parentNode;
+  removeProduto.parentNode.removeChild(removeProduto);
+  
+  if (produtosLocalStorage !== null){
+    var produtos = JSON.parse(produtosLocalStorage);
+    produtos.splice(artigo,1);
+    localStorage.setItem("produtos", JSON.stringify(produtos));
+  }
+  
+  var campoTotal = document.querySelector("#campoTotal");
+  var totalAtual = parseFloat(campoTotal);
+  var precoRemovido = parseFloat(produtosLocalStorage[artigo].preco); 
+  
+  campoTotal.textContent = ` ${(totalAtual - precoRemovido).toFixed(2)}`;
+}
 
 
 
--- js/server.js
+++ js/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -12,21 +12,28 @@ app.use(cors());
 app.post("/store-email", (req, res) => {
   const email = req.body.email;
 
-  if (!email) {
-    return res.status(400).json({ error: "Invalid email" });
-  }
-  
-  
-  const jsonData = email + ", ";
+  const jsonData = JSON.stringify(email);
   const filePath = path.join(__dirname, "emails.json");
-  
-  fs.appendFile(filePath, jsonData, (err) => {
+
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      console.error("Error writing to file:", err);
+      console.error("Error reading file:", err);
       return res.status(500).json({ error: "Error storing email" });
     }
+  
+    let emails = [];
+      if (data) {
+        emails = JSON.parse(data);
+      }
     
-    res.json({ message: "Email stored successfully" });
+    emails.push(email);
+
+    fs.writeFile(filePath, JSON.stringify(emails), (err) => {
+      if (err) {
+        console.log("Algo correu mal! Erro:", err);
+      }
+      res.json({ message: "Email stored successfully" });
+    });
   });
 });
 
@@ -41,4 +48,4 @@ app.listen(port, () => {
 /*Caso seja necessário parar o servidor pode correr: tasklist para ver qual o PID 
 que o node está a ocupar e de seguida correr: taskkill /F /PID <pid> */
 
-//De seguida correr: node js/server.js para iniciar o servidor
+/*De seguida correr: node js/server.js para iniciar o servidor*/
